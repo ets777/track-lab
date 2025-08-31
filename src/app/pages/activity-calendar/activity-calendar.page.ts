59da9967798b0d80fe17b03335e5c0e5a64edcf9
Dmitry Klishev <dklishev@astrosoft.ru>
2025-08-31 21:22:05 +0700

    chips for stats

--- src/app/pages/activity-calendar/activity-calendar.page.ts
+++ src/app/pages/activity-calendar/activity-calendar.page.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ActivityService } from '../../services/activity.service';
 import { IActivity } from 'src/app/db';
-import { addDays, format } from 'date-fns';
+import { addDays, addMonths, format } from 'date-fns';
 
 type NumberKeys<T> = {
   [K in keyof T]: T[K] extends number ? K : never
 }[keyof T];
 
 type ActivityNumberKeys = Exclude<NumberKeys<IActivity>, undefined>;
+
+type Period = 'week' | 'month';
 
 @Component({
   selector: 'app-activity-calendar',
@@ -23,11 +25,21 @@ export class ActivityCalendarPage implements OnInit {
   endDate: string = '';
   activities: IActivity[] = [];
   activitiesGroupedByDate: { date: string, activities: IActivity[] }[] = [];
+  selectedPeriod: Period = 'week';
 
   constructor(private activityService: ActivityService) { }
 
-  async ngOnInit() {
-    this.startDate = format(addDays(new Date(), -6), 'yyyy-MM-dd');
+  ngOnInit() {
+    this.setDefaultDates();
+  }
+
+  setDefaultDates() {
+    if (this.selectedPeriod == 'week') {
+      this.startDate = format(addDays(new Date(), -6), 'yyyy-MM-dd');
+    } else {
+      this.startDate = format(addMonths(new Date(), -1), 'yyyy-MM-dd');
+    }
+
     this.endDate = format(new Date(), 'yyyy-MM-dd');
   }
 
@@ -63,5 +75,24 @@ export class ActivityCalendarPage implements OnInit {
       .reduce((previousValue, currentValue) => currentValue + previousValue, 0);
 
     return sum / filteredValues.length;
+  }
+
+  async selectPeriod(period: Period) {
+    this.selectedPeriod = period;
+
+    this.setDefaultDates();
+    await this.loadStats();
+  }
+
+  async shiftDates(shift: number) {
+    if (this.selectedPeriod == 'week') {
+      this.startDate = format(addDays(new Date(this.startDate), shift * 7), 'yyyy-MM-dd');
+      this.endDate = format(addDays(new Date(this.endDate), shift * 7), 'yyyy-MM-dd');
+    } else {
+      this.startDate = format(addMonths(new Date(this.startDate), shift * 1), 'yyyy-MM-dd');
+      this.endDate = format(addMonths(new Date(this.endDate), shift * 1), 'yyyy-MM-dd');
+    }
+
+    await this.loadStats();
   }
 }
