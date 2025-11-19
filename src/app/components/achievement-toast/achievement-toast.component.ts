f7a3781bcdba29641867eafb7603655d7e6ad089
Dmitry Klishev <dklishev@astrosoft.ru>
2025-11-19 21:14:42 +0700

    fix toasts overlapping

--- src/app/components/achievement-toast/achievement-toast.component.ts
+++ src/app/components/achievement-toast/achievement-toast.component.ts
@@ -3,31 +3,31 @@ import { TranslateModule } from '@ngx-translate/core';
 import { AchievementService } from 'src/app/services/achievement.service';
 
 @Component({
-    selector: 'app-achievement-toast',
-    templateUrl: './achievement-toast.component.html',
-    styleUrls: ['./achievement-toast.component.scss'],
-    imports: [TranslateModule],
+  selector: 'app-achievement-toast',
+  templateUrl: './achievement-toast.component.html',
+  styleUrls: ['./achievement-toast.component.scss'],
+  imports: [TranslateModule],
 })
 export class AchievementToastComponent implements OnInit {
-    title = '';
-    icon = '';
-    visible = false;
+  title = '';
+  icon = '';
+  visible = false;
 
-    constructor(private achievementService: AchievementService) { }
+  constructor(private achievementService: AchievementService) { }
 
-    ngOnInit() {
-        this.achievementService.onEvent().subscribe(
-            (achievement) => {
-                this.show(achievement.title, achievement.icon ?? '🏆');
-            }
-        );
-    }
+  ngOnInit() {
+    this.achievementService.onEvent().subscribe(
+      (achievement) => {
+        this.show(achievement.title, achievement.icon ?? '🏆');
+      }
+    );
+  }
 
-    show(title: string, icon: string) {
-        this.title = title;
-        this.icon = icon;
-        this.visible = true;
-        setTimeout(() => this.visible = false, 5000);
-    }
+  show(title: string, icon: string) {
+    this.title = title;
+    this.icon = icon;
+    this.visible = true;
+    setTimeout(() => this.visible = false, 5000);
+  }
 
 }
--- src/app/components/toast/toast.component.ts
+++ src/app/components/toast/toast.component.ts
@@ -3,54 +3,54 @@ import { TranslateModule } from '@ngx-translate/core';
 import { IToast, ToastService } from 'src/app/services/toast.service';
 
 @Component({
-    selector: 'app-toast',
-    templateUrl: './toast.component.html',
-    styleUrls: ['./toast.component.scss'],
-    imports: [TranslateModule],
+  selector: 'app-toast',
+  templateUrl: './toast.component.html',
+  styleUrls: ['./toast.component.scss'],
+  imports: [TranslateModule],
 })
 export class ToastComponent implements OnInit {
-    title = '';
-    icon = '';
-    visible = false;
+  title = '';
+  icon = '';
+  visible = false;
 
-    constructor(private toastService: ToastService) { }
+  constructor(private toastService: ToastService) { }
 
-    ngOnInit() {
-        this.toastService.onEvent().subscribe(
-            (toast) => {
-                this.show(toast);
-            }
-        );
+  ngOnInit() {
+    this.toastService.onEvent().subscribe(
+      (toast) => {
+        this.show(toast);
+      }
+    );
+  }
+
+  show(toast: IToast) {
+    this.title = toast.title;
+    if (toast.icon) {
+      this.icon = toast.icon;
+    } else if (toast.type) {
+      switch (toast.type) {
+        case 'success':
+          this.icon = '✅';
+          break;
+        case 'error':
+          this.icon = '❌';
+          break;
+        case 'warning':
+          this.icon = '⚠️';
+          break;
+        case 'info':
+          this.icon = 'ℹ️';
+          break;
+        case 'waiting':
+          this.icon = '⏳';
+          break;
+      }
     }
-
-    show(toast: IToast) {
-        this.title = toast.title;
-        if (toast.icon) {
-            this.icon = toast.icon;
-        } else if (toast.type) {
-            switch(toast.type) {
-                case 'success':
-                    this.icon = '✅';
-                    break;
-                case 'error':
-                    this.icon = '❌';
-                    break;
-                case 'warning':
-                    this.icon = '⚠️';
-                    break;
-                case 'info':
-                    this.icon = 'ℹ️';
-                    break;
-                case 'waiting':
-                    this.icon = '⏳';
-                    break;
-            }
-        }
-        this.visible = true;
-        setTimeout(
-            () => this.visible = false, 
-            toast.duration ?? this.toastService.getDefaultDuration(),
-        );
-    }
+    this.visible = true;
+    setTimeout(
+      () => this.visible = false,
+      toast.duration ?? this.toastService.getDefaultDuration(),
+    );
+  }
 
 }
