e35f91934af45d1196b95533b677706a70937b7c
Dmitry Klishev <dklishev@astrosoft.ru>
2025-12-21 09:52:03 +0700

    metric adding button added

--- src/app/pages/metric-list/metric-list.page.ts
+++ src/app/pages/metric-list/metric-list.page.ts
@@ -1,20 +1,21 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonList, IonItem, IonLabel, IonFab, IonFabButton, IonIcon } from '@ionic/angular/standalone';
 import { TranslateModule } from '@ngx-translate/core';
 import { MetricService } from 'src/app/services/metric.service';
 import { IMetric } from 'src/app/db/models/metric';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-metric-list',
   templateUrl: './metric-list.page.html',
   styleUrls: ['./metric-list.page.scss'],
-  standalone: true,
-  imports: [IonLabel, IonItem, IonList, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuButton, TranslateModule],
+  imports: [IonIcon, IonFabButton, IonFab, IonLabel, IonItem, IonList, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuButton, TranslateModule],
 })
 export class MetricListPage {
   private metricService = inject(MetricService);
+  private router = inject(Router);
 
   metrics: IMetric[] = [];
 
@@ -24,5 +25,9 @@ export class MetricListPage {
 
   async fetchMetrics() {
     this.metrics = await this.metricService.getAll();
+  }
+
+  async goToAddPage() {
+    await this.router.navigate(['/metric/add']);
   }
 }
