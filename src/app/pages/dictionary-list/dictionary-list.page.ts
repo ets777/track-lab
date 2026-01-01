fad537dec1017ae82e3e30779e069e89ed99d857
Dmitry Klishev <dmitry.klishev@gmail.com>
2026-01-01 13:08:49 +0700

    empty dictionary list message

--- src/app/pages/dictionary-list/dictionary-list.page.ts
+++ src/app/pages/dictionary-list/dictionary-list.page.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonList, IonItem, IonLabel, IonMenuButton, IonFab, IonFabButton, IonIcon } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonList, IonItem, IonLabel, IonMenuButton, IonFab, IonFabButton, IonIcon, IonText } from '@ionic/angular/standalone';
 import { DictionaryService } from 'src/app/services/dictionary.service';
 import { IDictionary } from 'src/app/db/models/dictionary';
 import { TranslateModule } from '@ngx-translate/core';
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
   selector: 'app-dictionary-list',
   templateUrl: './dictionary-list.page.html',
   styleUrls: ['./dictionary-list.page.scss'],
-  imports: [IonIcon, IonFabButton, IonFab, IonLabel, IonItem, IonList, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuButton, TranslateModule],
+  imports: [IonText, IonIcon, IonFabButton, IonFab, IonLabel, IonItem, IonList, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuButton, TranslateModule],
 })
 export class DictionaryListPage {
   private dictionaryService = inject(DictionaryService);
