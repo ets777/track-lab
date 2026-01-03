15f56c5b99d5e07014af785dc57a677856194ab9
Dmitry Klishev <dmitry.klishev@gmail.com>
2026-01-03 23:23:54 +0700

    edit action formatting fixed

--- src/app/pages/action-edit/action-edit.page.ts
+++ src/app/pages/action-edit/action-edit.page.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, OnInit, ViewChild, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, ViewChild, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonButtons } from '@ionic/angular/standalone';
